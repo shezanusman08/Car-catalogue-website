4d7d55453c2fb1cea40237120256d5385f7f4c3f
shezanusman08 <164097373+shezanusman08@users.noreply.github.com>
2024-10-05 13:35:44 +0500

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        
       </body>
     </html>
   );
